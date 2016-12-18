997e65fe1e8bec38d655f18c85f820f4ae084a90
phearun008 <rathphearun123@gmail.com>
2016-12-18 17:42:37 +0700

    first comit

---
+++ src/main/java/com/phearun/SpringDemo01Application.java
@@ -0,0 +1,12 @@
+package com.phearun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo01Application.class, args);
+	}
+}
